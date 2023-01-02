b6c051486fe9dc116c57d576f21cbf1e515e4258
Amber-DiNenna <amberdinenna@gmail.com>
2023-01-02 14:12:06 -0800

    Add pw file.

---
+++ emailer/app2.py
@@ -0,0 +1,1 @@
+password = 'cntglsuvalhjgfxz'
--- emailer/emailer.py
+++ emailer/emailer.py
@@ -1,12 +1,12 @@
 from email.message import EmailMessage
 import ssl
 import smtplib
+from app2 import password
 
-password = 'cntglsuvalhjgfxz'
+
 
 email_sender = 'senormoments@gmail.com'
 email_password = password
-
 email_receiver = 'senormoments@gmail.com'
 
 subject = 'What Up'
@@ -24,4 +24,4 @@ context = ssl.create_default_context()
 
 with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
   smtp.login(email_sender, email_password)
-  smtp.sendmail
+  smtp.sendmail(email_sender, email_receiver, em.as_string())
