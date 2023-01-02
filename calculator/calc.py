eaa88e9ea609ee5564c5153c3c8a50d55922a29b
Amber-DiNenna <amberdinenna@gmail.com>
2023-01-02 14:46:24 -0800

    Define basic calculator functions.

--- calculator/calc.py
+++ calculator/calc.py
@@ -3,3 +3,25 @@
 # ask for values
 # call the functions
 # while loop to continue the program until the user wants to exit
+
+def add(a, b):
+  answer = a + b
+  print(f'{a} + {b} = {answer}')
+
+def sub(a, b):
+  answer = a - b
+  print(f'{a} - {b} = {answer}')
+
+def mult(a, b):
+  answer = a * b
+  print(f'{a} * {b} = {answer}')
+
+def div(a, b):
+  answer = a / b
+  print(f'{a} / {b} = {answer}')
+
+
+add(5, 10)
+sub(5, 10)
+mult(5, 10)
+div(5, 10)
