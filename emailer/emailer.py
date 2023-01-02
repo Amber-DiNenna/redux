6f81556b45b890e169731179e8daa64de41d2472
Amber-DiNenna <amberdinenna@gmail.com>
2023-01-02 14:13:27 -0800

    Add note.

--- emailer/emailer.py
+++ emailer/emailer.py
@@ -25,3 +25,6 @@ context = ssl.create_default_context()
 with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
   smtp.login(email_sender, email_password)
   smtp.sendmail(email_sender, email_receiver, em.as_string())
+
+
+### THIS EXAMPLE DIDNT WORK ###
