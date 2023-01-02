d3cdd0b580c1ef4e6d79cfa278f8111b06fc7638
Amber-DiNenna <amberdinenna@gmail.com>
2023-01-02 15:11:18 -0800

    Add while loop and error handling for user selection error.

--- calculator/calc.py
+++ calculator/calc.py
@@ -21,7 +21,51 @@ def div(a, b):
   print(f'{a} / {b} = {answer}')
 
 
-add(5, 10)
-sub(5, 10)
-mult(5, 10)
-div(5, 10)
+# add(5, 10)
+# sub(5, 10)
+# mult(5, 10)
+# div(5, 10)
+
+user_selection = input('''
+Please select your desired function from the following menu options:\n
+A. Addition
+B. Subtraction
+C. Multiplication
+D. Division
+E. Exit\n
+''')
+
+while True:
+  if user_selection == 'a' or user_selection == 'A':
+    print('\nAddition\n')
+    a = int(input('Enter first number: '))
+    b = int(input('Enter second number: '))
+    add(a, b)
+  elif user_selection == 'b' or user_selection == 'B':
+    print('\nSubtraction\n')
+    a = int(input('Enter first number: '))
+    b = int(input('Enter second number: '))
+    sub(a, b)
+  elif user_selection == 'c' or user_selection == 'C':
+    print('\nMultiplication\n')
+    a = int(input('Enter first number: '))
+    b = int(input('Enter second number: '))
+    mult(a, b)
+  elif user_selection == 'd' or user_selection == 'D':
+    print('\nDivision\n')
+    a = int(input('Enter first number: '))
+    b = int(input('Enter second number: '))
+    div(a, b)
+  elif user_selection == 'e' or user_selection == 'E':
+    print('\nFINE BYE.\n')
+    break
+  else:
+    print('\nPlease enter a valid letter choice from the menu options listed above.\n')
+    user_selection = input('''
+      Please select your desired function from the following menu options:\n
+      A. Addition
+      B. Subtraction
+      C. Multiplication
+      D. Division
+      E. Exit\n
+      ''')
