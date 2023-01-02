71add81d52b7dea7cedb48e030fff02356054dbe
Amber-DiNenna <amberdinenna@gmail.com>
2023-01-02 15:49:58 -0800

    Create new directory and file for binary search program with starter notes.

---
+++ search/binary_search.py
@@ -0,0 +1,7 @@
+# splits list data in two by index position
+# [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# works by adding first index position [0] and last index position [9] and dividing the result by 2 (= 4.5)
+# (0+9)/2 = 4.5
+# rounds off?
+# compares to target value
+# if target value = 3, it compares it to ___ in the list and discards the other half of the list values until it reaches the target?
