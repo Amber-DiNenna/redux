8e8f8d5b72182cc7f5e34ad4dc0f7ab4e51cfd95
Amber-DiNenna <amberdinenna@gmail.com>
2023-01-02 14:21:35 -0800

    Create directory and file for word replacer program, enter test code.

---
+++ replacer/replacer.py
@@ -0,0 +1,7 @@
+def replace_word():
+  str = 'suck me sideways'
+  word_to_replace = input("Enter the word to be replaced: ")
+  word_replacement = input("Enter the replacement word: ")
+  print(str.replace(word_to_replace, word_replacement))
+
+replace_word()
