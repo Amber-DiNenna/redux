de3a562ec4352084c97b4f698a92f641e5a05dba
Amber-DiNenna <amberdinenna@gmail.com>
2023-01-02 15:37:50 -0800

    Define main function that slices parts of user submitted input, saves each to a variable and returns to user.

--- slicer/slicer.py
+++ slicer/slicer.py
@@ -1,3 +1,22 @@
 # collect email from user
 # split email using the @, the first part as the user name, the second part as domain
 # split domain using the .
+
+
+def main():
+  print('''
+  Welcome to the Email Slicer
+  ''')
+
+  email_input = input('Enter your email address: ')
+
+  (username, domain) = email_input.split('@')
+  (domain, extension) = domain.split('.')
+
+  print(f'''
+  Username: {username}
+  Domain: {domain}
+  Extension: {extension}
+  ''')
+
+main()
